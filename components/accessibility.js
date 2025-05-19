01cd6b990fd654aeead0b00b24e84b8b99dd55d1
Bryan Widjaja <crz.github@gmail.com>
2025-05-19 23:18:32 +0700

    update accessibility

--- components/accessibility.js
+++ components/accessibility.js
@@ -2,6 +2,13 @@ const accessibilityBtn = document.getElementById("accessability-btn");
 const accessibilityMenu = document.querySelector(".accessibility-menu");
 const closeBtn = document.getElementById("acc-close-btn");
 
+const darkmodeBtn = document.getElementById("darkmode-btn");
+const smalltextBtn = document.getElementById("smalltext-btn");
+const bigtextBtn = document.getElementById("bigtext-btn");
+const saturationBtn = document.getElementById("saturation-btn");
+const disleksiaBtn = document.getElementById("disleksia-btn");
+const sundaBtn = document.getElementById("sunda-btn");
+
 const listenBtn = (btn) => {
   btn.addEventListener("click", () => {
     const isMenuVisible = accessibilityMenu.classList.toggle("visible");
@@ -14,3 +21,27 @@ const listenBtn = (btn) => {
 
 listenBtn(accessibilityBtn);
 listenBtn(closeBtn);
+
+const listenBtnToFunction = (btn, func) => {
+  btn.addEventListener("click", () => {
+    func();
+  });
+};
+
+const increaseTextSize = () => {
+  const rootFontSize = parseFloat(
+    getComputedStyle(document.documentElement).fontSize
+  );
+  document.querySelectorAll("body, body *").forEach((element) => {
+    const computedSize = getComputedStyle(element).fontSize;
+    const pxValue = parseFloat(computedSize);
+
+    if (!isNaN(pxValue)) {
+      const currentRem = pxValue / rootFontSize;
+      const newRem = currentRem * 1.1;
+      element.style.fontSize = newRem + "rem";
+    }
+  });
+};
+
+listenBtnToFunction(bigtextBtn, increaseTextSize);
