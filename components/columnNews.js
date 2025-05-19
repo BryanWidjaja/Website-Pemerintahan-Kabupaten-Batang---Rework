8c6ddd60164b4571048791c0d8d2050369d7d1d9
Bryan Widjaja <crz.github@gmail.com>
2025-05-19 21:39:06 +0700

    full-news page update

--- components/columnNews.js
+++ components/columnNews.js
@@ -3,14 +3,14 @@ const basePath =
     ? "/Website-Pemerintahan-Kabupaten-Batang---Rework"
     : "";
 
-const renderNews = (newsRowID, category = null) => {
+const renderNews = (newsColumnID, category = null) => {
   fetch(`${basePath}/assets/json/news.json`)
     .then((res) => res.json())
     .then((data) => {
       const categories = data.categories;
-      const parentElement = document.querySelector(`#${newsRowID}`);
+      const parentElement = document.querySelector(`#${newsColumnID}`);
       if (!parentElement) {
-        console.error(`Parent element #${newsRowID} not found`);
+        console.error(`Parent element #${newsColumnID} not found`);
         return;
       }
 
@@ -39,33 +39,23 @@ const renderNews = (newsRowID, category = null) => {
       parentElement.innerHTML = "";
 
       articles.forEach((article) => {
+        if (article.title === title) return;
         const container = document.createElement("div");
         container.classList.add("berita-container");
         container.innerHTML = `
-          <img src="./assets/images/berita/${article.img.src}.jpg" alt="${
+          <img src="../assets/images/berita/${article.img.src}.jpg" alt="${
           article.title
         }" class="berita-img" />
-          <a href="./pages/berita.html?title=${encodeURIComponent(
-            article.title
-          )}" class="berita-title">${article.title}</a>
-          <div class="berita-info-row">
+        <div class="berita-text-container">
+            <a href="./berita.html?title=${encodeURIComponent(
+              article.title
+            )}" class="berita-title">${article.title}</a>
+            
             <div class="berita-info-container">
-              <img src="./assets/icons/date.svg" alt="date : " class="berita-info-icon" />
-              <p class="berita-date">${article.date}</p>
+                <img src="../assets/icons/date.svg" alt="date : " class="berita-info-icon" />
+                <p class="berita-date">${article.date}</p>
             </div>
-            <div class="berita-info-container">
-              <img src="./assets/icons/user.svg" alt="writer : " class="berita-info-icon" />
-              <p class="berita-writer">${article.writer}</p>
-            </div>
-            <div class="berita-info-container">
-              <img src="./assets/icons/tag.svg" alt="category : " class="berita-info-icon" />
-              <a href="./pages/berita-list.html" class="berita-category">${
-                article.category
-              }</a>
-            </div>
-          </div>
-          <div class="horizontal-line"></div>
-          <p class="berita-content">${article.content.split("\n\n")[0]}</p>
+        </div>
         `;
 
         parentElement.appendChild(container);
@@ -74,4 +64,4 @@ const renderNews = (newsRowID, category = null) => {
     .catch((error) => console.error("Fetch error:", error));
 };
 
-renderNews("terbaru-row");
+renderNews("berita-lainnya");
--- components/fullNews.js
+++ components/fullNews.js
@@ -1,56 +1,60 @@
 const params = new URLSearchParams(window.location.search);
 const title = params.get("title");
 
-fetch("../assets/json/news.json")
-  .then((res) => res.json())
-  .then((data) => {
-    const allArticles = [];
-    Object.keys(data.categories).forEach((category) => {
-      data.categories[category].forEach((article) => {
-        allArticles.push({ ...article, category });
+const renderMainNews = (containerClass) => {
+  fetch("../assets/json/news.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const allArticles = [];
+      Object.keys(data.categories).forEach((category) => {
+        data.categories[category].forEach((article) => {
+          allArticles.push({ ...article, category });
+        });
       });
-    });
 
-    const article = allArticles.find((a) => a.title === title);
+      const article = allArticles.find((a) => a.title === title);
 
-    const container = document.querySelector(".berita-container");
+      const container = document.querySelector(`.${containerClass}`);
 
-    container.innerHTML = `
-      <div class="berita-directory-container">
-        <a href="../index.html" class="berita-directory">Home</a>
-        <p class="berita-directory-separator">/</p>
-        <a href="./berita-list.html" class="berita-directory">Berita</a>
-        <p class="berita-directory-separator">/</p>
+      container.innerHTML = `
+      <div class="main-berita-directory-container">
+        <a href="../index.html" class="main-berita-directory">Home</a>
+        <p class="main-berita-directory-separator">/</p>
+        <a href="./berita-list.html" class="main-berita-directory">Berita</a>
+        <p class="main-berita-directory-separator">/</p>
         <a href="./berita-list.html#${
           article.category
-        }" class="berita-directory">${article.category}</a>
+        }" class="main-berita-directory">${article.category}</a>
       </div>
-      <p class="berita-title">${article.title}</p>
-      <div class="berita-info-row">
-        <div class="berita-info-container">
-          <img src="../assets/icons/date.svg" alt="date : " class="berita-info-icon" />
-          <p class="berita-date">${article.date}</p>
+      <p class="main-berita-title">${article.title}</p>
+      <div class="main-berita-info-row">
+        <div class="main-berita-info-container">
+          <img src="../assets/icons/date.svg" alt="date : " class="main-berita-info-icon" />
+          <p class="main-berita-date">${article.date}</p>
         </div>
-        <div class="berita-info-container">
-          <img src="../assets/icons/user.svg" alt="writer : " class="berita-info-icon" />
-          <p class="berita-writer">${article.writer}</p>
+        <div class="main-berita-info-container">
+          <img src="../assets/icons/user.svg" alt="writer : " class="main-berita-info-icon" />
+          <p class="main-berita-writer">${article.writer}</p>
         </div>
-        <div class="berita-info-container">
-          <img src="../assets/icons/tag.svg" alt="category : " class="berita-info-icon" />
-          <a href="./berita-list.html" class="berita-category">${
+        <div class="main-berita-info-container">
+          <img src="../assets/icons/tag.svg" alt="category : " class="main-berita-info-icon" />
+          <a href="./berita-list.html" class="main-berita-category">${
             article.category
           }</a>
         </div>
       </div>
       <img src="../assets/images/berita/${
         article.img.src
-      }.jpg" class="berita-img"alt="${article.title}" />
-      <p class="berita-content">${article.content.replace(
+      }.jpg" class="main-berita-img"alt="${article.title}" />
+      <p class="main-berita-content">${article.content.replace(
         /\n\n/g,
         "<br><br>"
       )}</p>
     `;
-  })
-  .catch((error) => {
-    console.error("Fetch error:", error);
-  });
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
+};
+
+renderMainNews("main-berita-container");
