c09c4b6480ee4190c9a8d95f1b151484ba16d13a
Bryan Widjaja <crz.github@gmail.com>
2025-06-03 00:21:05 +0700

    init carousel

--- components/index.js
+++ components/index.js
@@ -1,3 +1,3 @@
 renderNews("terbaru-row", null, false, javaneseActive);
-renderPhotoNews("berita-foto-row-1", null, false, 0, 3);
-renderPhotoNews("berita-foto-row-2", null, false, 3, 6);
+// renderPhotoNews("berita-foto-row-1", null, false, 0, 3);
+// renderPhotoNews("berita-foto-row-2", null, false, 3, 6);
